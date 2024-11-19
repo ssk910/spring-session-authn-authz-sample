b8a0144cb81606cf8af3acb0e441868912331001
Chris <hanmir91@gmail.com>
2024-11-19 15:43:03 +0900

    chore: Add initial files

---
+++ src/main/java/com/sparta/spring_session_authn_authz_sample/SpringSessionAuthnAuthzSampleApplication.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_session_authn_authz_sample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSessionAuthnAuthzSampleApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SpringSessionAuthnAuthzSampleApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/sparta/spring_session_authn_authz_sample/SpringSessionAuthnAuthzSampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sparta.spring_session_authn_authz_sample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringSessionAuthnAuthzSampleApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
